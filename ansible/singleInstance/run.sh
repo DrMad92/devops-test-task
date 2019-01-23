35ceaa5f1f43110b02225dad0d257b25cca10f70
DrMad92 <mmadraimov@gmail.com>
2019-01-24 00:55:02 +0200

    Update run.sh

--- ansible/singleInstance/run.sh
+++ ansible/singleInstance/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ansible-playbook --ask-vault-pass infrastructure.yaml;
-ansible-playbook -i ec2_hosts deploy.yaml;
+ansible-playbook --ask-vault-pass -i ec2_hosts deploy.yaml;
 echo "${SECONDS} elapsed"
