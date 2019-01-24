7b3a36f880c542ae9b0c4109eb94267bbee997b8
DrMad92 <mmadraimov@gmail.com>
2019-01-24 03:29:02 +0200

    Update deploy.yml and run.sh [ci-skip]

--- ansible/singleInstance/run.sh
+++ ansible/singleInstance/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-ansible-playbook --ask-vault-pass infrastructure.yaml;
-ansible-playbook --ask-vault-pass -i ec2_hosts deploy.yaml;
+echo "Enter vault password"
+read -s VAULT_PASS
+mkfifo -m 600 ./vault_pass
+
+echo ${VAULT_PASS} > ./vault_pass &
+ansible-playbook  infrastructure.yaml --vault-password-file=./vault_pass;
+echo ${VAULT_PASS} > ./vault_pass &
+ansible-playbook -i ec2_hosts deploy.yaml --vault-password-file=./vault_pass;
+
+rm -f ./vault_pass
 echo "${SECONDS} elapsed"
